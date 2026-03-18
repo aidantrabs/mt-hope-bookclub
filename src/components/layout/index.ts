e98a3dd6775bc26c16e0ddf9a24252747dcece26
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-03-18 15:44:19 -0400

    refactor: remove barrel files and replace with direct imports

--- src/components/layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { Footer } from "./footer.tsx";
-export { Header } from "./header.tsx";
-export { PageWrapper } from "./page-wrapper.tsx";
-export { ProtectedRoute } from "./protected-route.tsx";
--- src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-export { About } from "./about.tsx";
-export { BookForm } from "./admin/book-form.tsx";
-export { Dashboard } from "./admin/dashboard.tsx";
-export { Login } from "./admin/login.tsx";
-export { BookDetail } from "./book-detail.tsx";
-export { Books } from "./books.tsx";
-export { Contact } from "./contact.tsx";
-export { Home } from "./home.tsx";
